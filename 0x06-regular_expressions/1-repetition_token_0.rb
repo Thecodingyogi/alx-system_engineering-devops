70e11d2907b49d8e5933352703ce2100f719c1de
Thecodingyogi <cynthiatonui@gmail.com>
2023-05-03 13:30:29 -0700

    Regular expression tasks

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#A Ruby script that accepts one argument
+
+puts ARGV[0].scan(/hbt{2,5}/n).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+#A Ruby script that accepts one argument and pass it to a regular expression matching method
+#The regex should not contain square brackets
+
+puts ARGV[0].scan(/hbt+n/).join
