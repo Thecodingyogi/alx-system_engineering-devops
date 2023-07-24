146db5fa5bc1a696921e43cae0da3a94e771db35
Thecodingyogi <cynthiatonui@gmail.com>
2023-07-24 03:50:55 -0700

    Used formatting instead of fstrings

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     completed_tasks = [todo for todo in todos if todo["completed"]]
     num_completed_tasks = len(completed_tasks)
 
-    print(f"Employee {user_name} is done with tasks\
-            ({num_completed_tasks}/{number_of_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+                                                          user_name,
+                                                          num_completed_tasks,
+                                                          number_of_tasks))
     for task in completed_tasks:
         print(f"\t{task['title']}")
