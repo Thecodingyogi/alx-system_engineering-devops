829faecaf07953f2f3204044af3a7415a29b49fc
Thecodingyogi <cynthiatonui@gmail.com>
2023-08-08 13:04:05 -0700

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {
             "user-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
             }
-    response = requests.get(url, headers=headesr. allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         results = response.json().get("data")
         return results.get("subscribers")
