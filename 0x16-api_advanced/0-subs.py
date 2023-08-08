78394ecb84ef7c7c220e2cdb9cdeb56fc508d0f2
Thecodingyogi <cynthiatonui@gmail.com>
2023-08-08 03:23:33 -0700

    API Advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function to query the reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "user-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+    response = requests.get(url, headers=headesr. allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        return results.get("subscribers")
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    posts = response.json().get("data")
+    [print(post.get("data").get("title")) for post in posts.get("children")]
