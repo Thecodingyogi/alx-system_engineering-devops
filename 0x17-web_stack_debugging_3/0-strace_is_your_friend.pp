2ec5bb6d64598922be27df0a3c534cb8575687de
Thecodingyogi <cynthiatonui@gmail.com>
2023-08-09 13:28:55 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet code to fix a bug wp-sttings.php
 
-exec {
+exec { 'fix the php extension issue':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
